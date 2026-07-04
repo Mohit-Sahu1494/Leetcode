2834f95054de8780e82ba330c503d2ba439e49e9
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-04 12:51:45 +0530

    Time: 3 ms (15.97%), Space: 8 MB (4.79%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -8,10 +8,11 @@ public:
          int right=n;
           while(left<right){
                 int mid=left+(right-left)/2;
-
+            //     yah par agar Bad version true mila to yah bhi  ho sakta hai ki isse pahle bhi kahi badversion ho 
                 if(isBadVersion(mid)){
                     right=mid;
                 }
+                //  agar bad version false mila to yah hoga ki  is number se pahle kahi par bhi bad version  nhi hia
                 else{
                     left=mid+1;
                 }
