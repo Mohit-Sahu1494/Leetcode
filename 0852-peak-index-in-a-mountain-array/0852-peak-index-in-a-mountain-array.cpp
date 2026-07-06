96fcedb6af7ebeb5dd807df9d32a2fe16e3ac56e
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-06 12:40:41 +0530

    Time: 0 ms (100%), Space: 63.5 MB (48.24%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,14 +4,14 @@ public:
         int left=0;
         int right=arr.size()-1;
         int ans=-1;;
-        while(left<=right){
+        while(left<right){
             int mid=left+(right-left)/2;
             if(arr[mid]<arr[mid+1]){
             ans=mid+1;
             left=mid+1;
             }
             else{
-                right=mid-1;
+                right=mid;
             }
         }
         return ans;
