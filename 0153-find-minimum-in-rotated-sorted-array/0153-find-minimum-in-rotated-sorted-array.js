3a138e856414311104c8c56d6abdb89e7a23af1d
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-07 12:01:22 +0530

    Time: 0 ms (100%), Space: 53.7 MB (53.42%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+      let left=0;
+      let right=nums.length-1;
+      let ans=0;
+      while(left<=right){
+        let mid= left+Math.floor((right-left)/2);
+        if(nums[mid]<=nums[nums.length-1]){
+            ans=nums[mid];
+            right=mid-1;
+        }
+        else{
+            left=mid+1;
+        }
+
+      }
+
+  return ans;
+};
