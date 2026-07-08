2a7ffd47a4641bbf14e769dbaa2992fb055c952f
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-08 13:08:26 +0530

    Time: 0 ms (100%), Space: 12.6 MB (39.87%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -7,7 +7,6 @@ public:
         while(left<right){
             int mid= left+(right-left)/2;
             if(nums[mid]<=nums[mid+1]){
-                ans=mid+1;
                 left=mid+1;
             }
             else{
