4e67f6232b302fb88e9f70c155f093989d82db06
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-15 14:58:20 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.85%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int num = x;
+        long long reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        return reverse == x;
+    }
+};
