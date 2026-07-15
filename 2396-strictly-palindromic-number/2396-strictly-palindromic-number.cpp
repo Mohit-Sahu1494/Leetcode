bf0a37781e51e94c864f529d6f830603b2f38a1a
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-15 15:25:39 +0530

    Time: 0 ms (100%), Space: 7.7 MB (90.54%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
        for (int i = 2; i <= n - 2; i++) {
-    long long reverse = 0;
-    int num = n;
-    string s="";
-    while (num != 0) {
-        s.push_back((num%i)+'0');
-        num/=i;
-    }
+            long long reverse = 0;
+            int num = n;
+            string s="";
+            while (num != 0) {
+                s.push_back((num%i)+'0');
+                num/=i;
+            }
 
-    int l = 0;
-int r = s.length() - 1;
+            int l = 0;
+            int r = s.length() - 1;
 
-while (l < r) {
-    if (s[l] != s[r])
-        return false;
-    l++;
-    r--;
-}
+            while (l < r) {
+            if (s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
 
 
 
-}
+        }
 return true;
     }
 };
