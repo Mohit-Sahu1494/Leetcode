8576d26dd26b42355c5003ae67ef5b64ac344f3d
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-17 11:57:27 +0530

    Time: 0 ms (100%), Space: 13.8 MB (8.86%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode *curr=head;
-        ListNode *prev=nullptr;
-        ListNode *agla=nullptr;
-
-        while(curr!=nullptr){
-            agla=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=agla;
+    ListNode* reverse(ListNode*head){
+        if( head==nullptr||head->next==nullptr){
+            return head;
         }
-      return prev;
+        ListNode* newList=reverse(head->next);
+        head->next->next=head;
+        head->next=nullptr;
+        return newList;
+    }
+    ListNode* reverseList(ListNode* head) {
+
+        //  yah iteration isko kiya hu 
+    //     ListNode *curr=head;
+    //     ListNode *prev=nullptr;
+    //     ListNode *agla=nullptr;
+
+    //     while(curr!=nullptr){
+    //         agla=curr->next;
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=agla;
+    //     }
+    //   return prev;
+
+    //    ab recoursive function se solve karenge .
+    ListNode* temp=reverse(head);
+        return temp;
+
     }
 };
