d89c85d06bbe2afd4fa01c884191a7d616468079
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-23 22:01:03 +0530

    Time: 0 ms (100%), Space: 9.7 MB (26.57%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+
+        string str = "";
+
+        for (char ch : s) {
+            str += to_string(ch - 'a' + 1);
+        }
+        while (k--) {
+            int sum = 0;
+
+            for (char ch : str) {
+                sum += ch - '0';
+            }
+
+            str = to_string(sum);
+        }
+
+        return stoi(str);
+    }
+};
