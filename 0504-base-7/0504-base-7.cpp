4d84e873a595f5c80adee71a23ff0d35116c515c
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-24 16:18:30 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.38%) - LeetHub

---
+++ 0504-base-7/0504-base-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   string convertToBase7(int num) {
+    if (num == 0) return "0";
+
+    bool negative = num < 0;
+    num = abs(num);
+
+    string ans = "";
+
+    while (num > 0) {
+        ans += char('0' + num % 7);
+        num /= 7;
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    if (negative)
+        ans = "-" + ans;
+
+    return ans;
+}
+};
