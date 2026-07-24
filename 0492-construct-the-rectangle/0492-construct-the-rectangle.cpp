b96701a5b86983a881c8f45695f96155ca136fd2
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-24 15:57:36 +0530

    Time: 218 ms (7.54%), Space: 8 MB (85.78%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+         int l;
+         int w;
+         int minimum=INT_MAX;
+        for(int width=1;width<=area;width++){
+            if(area%width!=0) continue;
+           int length=area/width;
+          if(length>=width){
+             int deff=length-width;
+             if(deff<minimum){
+                minimum=deff;
+                l=length;
+                w=width;
+             }
+          }
+        }
+        return {l,w};
+    }
+};
