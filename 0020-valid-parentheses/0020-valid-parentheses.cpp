8bdce8cfcc98c3882dac42e6cb6c02dddc28246d
Mohit Sahu <sahumohit1494@gmail.com>
2026-07-28 11:49:27 +0530

    Time: 0 ms (100%), Space: 10.1 MB (6.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<int> st;
-         
-         for(int i=0;i<s.length();i++){
-            if(s[i]=='('||s[i]=='['||s[i]=='{'){
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
                 st.push(s[i]);
-            }
-            else if(s[i]==']'||s[i]==')'||s[i]=='}'){
-                 if (st.empty()) {
-        return false;
-    }
-                if(st.top()=='(' &&s[i]==')'||st.top()=='[' &&s[i]==']'||st.top()=='{' &&s[i]=='}'){
-                    st.pop();
+            } else if (s[i] == ']' || s[i] == ')' || s[i] == '}') {
+                if (st.empty()) {
+                    return false;
                 }
-                else{
+                if (st.top() == '(' && s[i] == ')' ||
+                    st.top() == '[' && s[i] == ']' ||
+                    st.top() == '{' && s[i] == '}') {
+                    st.pop();
+                } else {
                     return false;
                 }
             }
-         }
+        }
         return st.empty();
     }
 };
