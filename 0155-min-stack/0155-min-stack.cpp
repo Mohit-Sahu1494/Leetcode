6cc7ca8902207efba39131511e6506f47184f6b9
Mohit Sahu <sahumohit1494@gmail.com>
2026-08-02 15:50:34 +0530

    Time: 46 ms (85.43%), Space: 151.3 MB (35.54%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,49 @@
+class MinStack {
+public:
+    stack<long long> st;
+    long long mini;
+
+    MinStack() {
+    }
+
+    void push(int value) {
+        if (st.empty()) {
+            st.push(value);
+            mini = value;
+        }
+        else if (value < mini) {
+
+            // encoded = 2 * newMin - oldMin
+            st.push(2LL * value - mini);
+
+            mini = value;
+        }
+        else {
+            st.push(value);
+        }
+    }
+
+    void pop() {
+        if (st.empty()) return;
+
+        long long topValue = st.top();
+        st.pop();
+
+        if (topValue < mini) {
+            // recover old minimum
+            mini = 2 * mini - topValue;
+        }
+    }
+
+    int top() {
+        if (st.top() < mini) {
+            return (int)mini;
+        }
+
+        return (int)st.top();
+    }
+
+    int getMin() {
+        return (int)mini;
+    }
+};
