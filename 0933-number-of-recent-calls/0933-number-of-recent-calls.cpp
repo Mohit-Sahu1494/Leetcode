79bccf7d74ca4ac4231e6931b86e2d45fcd553c9
Mohit Sahu <sahumohit1494@gmail.com>
2026-08-09 15:52:55 +0530

    Time: 21 ms (34.2%), Space: 64.2 MB (69.17%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -7,9 +7,8 @@ public:
     
     int ping(int t) {
         q.push(t);
-        while(q.front()<t-3000){
+        while(q.front()<t-3000)
             q.pop();
-        }
         return q.size();
     }
 };
