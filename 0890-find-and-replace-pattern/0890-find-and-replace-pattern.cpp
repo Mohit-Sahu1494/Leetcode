f38f5bcbdeb21378f52c77d3957c43a5d6146e0c
Mohit Sahu <sahumohit1494@gmail.com>
2026-08-11 12:30:49 +0530

    Time: 3 ms (39.48%), Space: 11.7 MB (40.08%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-   bool match(string word,string pattern){
-     if(word.length()!=pattern.length()){
-        return false;
-     }
+    bool match(string str, string pattern) {
+        if (str.length() != pattern.length()) return false;
 
-     unordered_map<char,char>mpWord;
-      unordered_map<char,char>mpPattern;
+        unordered_map<char, char> mpWord;
+        unordered_map<char, char> mpPattern;
 
-      for(int i=0;i<word.length();i++){
-         char w=word[i];
-         char p=pattern[i];
+        for (int i = 0; i < str.length(); i++) {
+            char w = str[i];
+            char p = pattern[i];
 
-         if(mpWord.count(p) && mpWord[p]!=w){
-            return false;
-         }
-         if(mpPattern.count(w) && mpPattern[w]!=p){
-            return false;
-         }
-        mpWord[p]=w;
-        mpPattern[w]=p;
-      }
-      return true;
+            if (mpWord.count(p) && mpWord[p] != w) return false;
+            if (mpPattern.count(w) && mpPattern[w] != p) return false;
 
-   }
+            mpWord[p] = w;
+            mpPattern[w] = p;
+        }
+
+        return true;
+    }
 
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        vector<string>ans;
+        vector<string> ans;
 
-        for(string str:words){
-            if(match(str,pattern)){
+        for (string str : words) {
+            if (match(str, pattern)) {
                 ans.push_back(str);
             }
         }
+
         return ans;
     }
 };
