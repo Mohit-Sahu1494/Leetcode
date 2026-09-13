6568aec3fb17670aeec5305bfceade06f3595f36
Mohit Sahu <sahumohit1494@gmail.com>
2026-09-13 21:59:07 +0530

    Time: 0 ms (100%), Space: 16.2 MB (70.67%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    int findGCD(vector<int>& nums){
-    int maxi=INT_MIN ;
-    int mini=INT_MAX;
-        for(int i=0;i<nums.size();i++){
-            maxi=max(maxi,nums[i]);
-            mini=min(mini,nums[i]);
-        }
-        return __gcd(maxi,mini);
+ int gcd(int a,int b){
+     while(b!=0){
+        int rem=a%b;
+    a=b;
+    b=rem;
+     }
+     return a;
+ }
+    int findGCD(vector<int>& nums) {
+         int small=INT_MAX;
+         int large=INT_MIN;
+
+         for(int i=0;i<nums.size();i++){
+            large=max(large,nums[i]);
+            small=min(small,nums[i]);
+         }
+        return gcd(large,small);   
     }
 };
